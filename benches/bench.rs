c9c51f94bf0bceef412e91009a8d2c21014a0da9
Clark Kampfe <clark.kampfe@gmail.com>
2020-02-26 15:24:49 -0600

    use a reader interface for ~30% less memory usage

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,26 +2,41 @@
 extern crate criterion;
 
 use criterion::Criterion;
-use memmap::MmapOptions;
 use nom_stl::parse_stl;
+use std::io::BufReader;
+
+fn parse_stl_binary_big(c: &mut Criterion) {
+    let root_vase_file = std::fs::File::open("./fixtures/Root_Vase.stl").unwrap();
+    let mut root_vase = BufReader::new(&root_vase_file);
+
+    let mut group = c.benchmark_group("big");
+
+    group.sample_size(15);
+
+    group.bench_function("parse_stl_root_vase_binary", move |b| {
+        b.iter(|| parse_stl(&mut root_vase))
+    });
+
+    group.finish();
+}
 
 fn parse_stl_binary(c: &mut Criterion) {
     let moon_file = std::fs::File::open("./fixtures/MOON_PRISM_POWER_binary.stl").unwrap();
-    let moon = unsafe { MmapOptions::new().map(&moon_file).unwrap() };
+    let mut moon = BufReader::new(&moon_file);
 
     c.bench_function("parse_stl_moon_prism_power_binary", move |b| {
-        b.iter(|| parse_stl(&moon))
+        b.iter(|| parse_stl(&mut moon))
     });
 }
 
 fn parse_stl_ascii(c: &mut Criterion) {
     let moon_file = std::fs::File::open("./fixtures/MOON_PRISM_POWER.stl").unwrap();
-    let moon = unsafe { MmapOptions::new().map(&moon_file).unwrap() };
+    let mut moon = BufReader::new(&moon_file);
 
     c.bench_function("parse_stl_moon_prism_power", move |b| {
-        b.iter(|| parse_stl(&moon))
+        b.iter(|| parse_stl(&mut moon))
     });
 }
 
-criterion_group!(benches, parse_stl_binary, parse_stl_ascii);
+criterion_group!(benches, parse_stl_binary_big, parse_stl_binary, parse_stl_ascii);
 criterion_main!(benches);
